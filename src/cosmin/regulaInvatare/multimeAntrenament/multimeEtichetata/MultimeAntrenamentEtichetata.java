09185d31c208512cc8b7155036a452780b096990
Cosmin2000B <cosmin.ionescu@s.utm.ro>
2022-04-17 21:04:15 +0300

    adaugat matrice etichete hardcodate

--- src/cosmin/regulaInvatare/multimeAntrenament/multimeEtichetata/MultimeAntrenamentEtichetata.java
+++ src/cosmin/regulaInvatare/multimeAntrenament/multimeEtichetata/MultimeAntrenamentEtichetata.java
@@ -11,12 +11,23 @@ import java.util.HashMap;
 public abstract class MultimeAntrenamentEtichetata extends MultimeAntrenament
 {
     private HashMap<Integer, String> corespondentaEticheta;
+    private ArrayList<ArrayList<Double>> etichete;
 
     public MultimeAntrenamentEtichetata(File locatieMemorie, HashMap<Integer, String> corespondentaEticheta)
     {
         super(locatieMemorie);
 
         this.corespondentaEticheta = corespondentaEticheta;
+
+        int nrClase = corespondentaEticheta.size();
+        this.etichete = new ArrayList<>(nrClase);
+        for(int i = 0; i < nrClase; ++i)
+        {
+            ArrayList<Double> eticheta =
+                    new ArrayList<>(Collections.nCopies(corespondentaEticheta.size(), 0d));
+            eticheta.set(i, 1d);
+            this.etichete.add(eticheta);
+        }
     }
 
     // --------- Setteri si Getteri -------------
@@ -36,9 +47,16 @@ public abstract class MultimeAntrenamentEtichetata extends MultimeAntrenament
     // hardcodarea unui vector de 0
     public ArrayList<Double> getEtichetaCorespunzatoare(int indexClasaElementului)
     {
-        ArrayList<Double> eticheta =
-                new ArrayList<>(Collections.nCopies(corespondentaEticheta.size(), 0d));
-        eticheta.set(indexClasaElementului, 1d);
-        return eticheta;
+        return etichete.get(indexClasaElementului);
+    }
+
+    public ArrayList<ArrayList<Double>> getEtichete()
+    {
+        return etichete;
+    }
+
+    public void setEtichete(ArrayList<ArrayList<Double>> etichete)
+    {
+        this.etichete = etichete;
     }
 }
