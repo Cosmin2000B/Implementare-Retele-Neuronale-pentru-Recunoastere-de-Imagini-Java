e223a6194d7abd08837a40791acd9f3729347308
Cosmin2000B <cosmin.ionescu@s.utm.ro>
2022-05-07 15:50:58 +0300

    adaugare informatii pachet neuron

---
+++ src/cosmin/neuron/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ *  Pachetul conține elementele necesare creării unui neuron artificial bazat pe
+ * modelul perceptronului definit de Frank Rosenblatt și interconectarea mai multor
+ * neuroni artificiali prin intermediul sinapselor.
+ * @since 1.0
+ * @author Ionescu Cosmin
+ * @version 1.0
+ */
+package cosmin.neuron;
