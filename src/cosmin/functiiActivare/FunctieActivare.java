921aee94cc5f831cf359e37999dc859cef9276e6
Cosmin2000B <cosmin.ionescu@s.utm.ro>
2022-05-08 01:03:04 +0300

    adaugat javadoc

--- src/cosmin/functiiActivare/FunctieActivare.java
+++ src/cosmin/functiiActivare/FunctieActivare.java
@@ -1,22 +1,34 @@
 package cosmin.functiiActivare;
 
 /**
+ *  Interfața definește comportamentul pe care orice funcție de activare trebuie
+ * să-l aibă.
+ *  Funcția de activare (de transfer) are rolul de a determina <strong>gradul
+ * de activare</strong> al unui neuron, prin intermediul ei putându-se controla,
+ * în funcție de dezideratul aplicației, interpretarea stării neuronului.
  * @author Ionescu Cosmin
  */
-
 public interface FunctieActivare
 {
     /**
-     *
-     * @param input
-     * @return
+     *  Metoda calculează valoarea funcției de activare pentru
+     * parametrul dat de utilizator.
+     * @param input reprezintă valoarea obținută în urma modelării
+     *              efectului cumulativ al semnalelor de intrare,
+     *              numită și <strong>starea neuronului artificial.
+     *              </strong>
+     * @return valoarea funcției de activare, numită și <strong>
+     *     gradul de activare al neuronului artificial.</strong>
      */
     public double valoareFunctie(Double input);
 
     /**
-     *
-     * @param input
-     * @return
+     *  Metoda calculează valoarea derivatei (de ordinul I) funcției
+     * de activare pentru parametrul dat de utilizator.
+     * @param input valoarea numerică pentru care se dorește calculul
+     *              derivatei.
+     * @return valoarea derivatei funcției de activare pentru parametrul
+     * dat de utilizator.
      */
     public double valoareDerivata(Double input);
 }
