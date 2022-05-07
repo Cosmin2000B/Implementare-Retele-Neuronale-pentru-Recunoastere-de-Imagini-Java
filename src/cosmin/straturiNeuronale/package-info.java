03442d52c8e04c9d406f95b0b001247ded08ae66
Cosmin2000B <cosmin.ionescu@s.utm.ro>
2022-05-08 02:07:42 +0300

    adaugare documentatie pachet

---
+++ src/cosmin/straturiNeuronale/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * <pr>
+ *   Pachetul oferă atât interpretări concrete ale straturilor neuronale
+ *  liniare utilizate în cadrul implementării unui perceptron multistrat,
+ *  cât și interfețe - clase abstracte cu rol sugestiv în definirea unor
+ *  noi implementări.
+ * </pr>
+ *
+ *  @see cosmin.straturiNeuronale.StratNeuronal
+ *  @see cosmin.straturiNeuronale.straturiNeuronaleLiniare.StratNeuronalLiniar
+ *  @since 1.0
+ *  @author Ionescu Cosmin
+ *  @version 1.0
+ */
+package cosmin.straturiNeuronale;
