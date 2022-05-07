4c2480409f8e01908f678b45d558c81631a37402
Cosmin2000B <cosmin.ionescu@s.utm.ro>
2022-05-08 01:23:21 +0300

    adaugare documentatie pachet

---
+++ src/cosmin/functiiActivare/package-info.java
@@ -0,0 +1,14 @@
+
+/**
+ *<pre>
+ *  Pachetul conține funcții de activare utilizate frecvent în măsurarea
+ * gradului de activare (calcularea valorii de ieșire) al unui neuron artificial.
+ *  În cazul în care utilizatorul dorește crearea unei noi funcții de activare,
+ * acesta trebuie să implementeze interfața FunctieActivare.
+ *</pre>
+ * @see cosmin.functiiActivare.FunctieActivare
+ * @since 1.0
+ * @author Ionescu Cosmin
+ * @version 1.0
+ */
+package cosmin.functiiActivare;
